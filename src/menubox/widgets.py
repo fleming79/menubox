e5f1171687ecd6bf655fe48f5b66d6761d5533b6
Alan Fleming <Alan.Fleming@utas.edu.au>
2025-05-18 11:53:16 +1000

    Add check_equality option to ValidateWidget

--- src/menubox/widgets.py
+++ src/menubox/widgets.py
@@ -13,6 +13,7 @@ import ipywidgets as ipw
 import traitlets
 from IPython import display as ipd
 
+import menubox
 from menubox import defaults, mb_async
 
 if TYPE_CHECKING:
@@ -52,13 +53,13 @@ class ValidateWidget(ipw.ValueWidget):
         1. overridden as a method, or
         2. specified using dynamic default with [traitlets.default](https://traitlets.readthedocs.io/en/stable/api.html#dynamic-default-values).
 
-    Note: A weakref is maintained to the validate function.
-    If subclassing, validate can be overloaded directly.
+    If subclassing, `validate` and `check_equality` can be overloaded as methods.
     """
 
     _skip_validate = False
     value = ValidatedTrait(None).tag(sync=True)
     _bi_validate: weakref.ref | None = None
+    check_equality: Callable[[Any, Any], bool]
 
     @traitlets.validate("value")
     def _validate_value(self, proposal: ProposalType):
@@ -66,12 +67,21 @@ class ValidateWidget(ipw.ValueWidget):
             return proposal["value"]
         return self.validate(proposal["value"])
 
-    def __init__(self, *, validate: None | Callable[[ProposalType], Any] = None, **kwargs):
+    def __init__(
+        self,
+        *,
+        validate: None | Callable[[ProposalType], Any] = None,
+        check_equality: Callable[[Any, Any], bool] | None = None,
+        **kwargs,
+    ):
         if validate:
             if inspect.ismethod(validate):
                 self._bi_validate = weakref.WeakMethod(validate)
             else:
                 self._bi_validate = lambda: validate  # type: ignore
+
+        if check_equality or not hasattr(self, "check_equality"):
+            self.check_equality = check_equality or menubox.HasParent.check_equality
 
         super().__init__(**kwargs)
 
@@ -81,14 +91,16 @@ class ValidateWidget(ipw.ValueWidget):
             fe_value = self._trait_from_json(sync_data["value"], self)
             try:
                 # Perform validation prior to setting it with the traitlets machinery making it straight forward
-                # to revert the original value back to the frontend should the validation fail.
-                sync_data["value"] = self._trait_to_json(self.validate(fe_value), self)
+                # to revert the original value back to the frontend should validation fail.
+                val = self.validate(fe_value)
+                sync_data["value"] = self._trait_to_json(val, self)
                 self._skip_validate = True
                 super().set_state(sync_data)
+                if not self.check_equality(fe_value, val):
+                    self.send_state("value")
             except Exception:
-                self.log.warning(
-                    "Validation failed for value= {fe_value}. Reverting state in the frontend.",
-                    extra={"fe_value": fe_value},
+                self.log.debug(
+                    f"Validation failed for value= {fe_value!r}. Reverting state in the frontend.",
                 )
                 self.send_state(tuple(sync_data))
             finally:
