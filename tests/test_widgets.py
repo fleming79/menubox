2ac6e9615eeadf1cf65e586e188344f1feec3cbb
Alan Fleming <>
2026-01-29 13:37:10 +1100

    Add test for DropdownAdd.

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -1,4 +1,4 @@
-from menubox.widgets import SelectMultipleValidate
+from menubox.widgets import DropdownAdd, SelectMultipleValidate
 
 
 async def test_SelectMultipleValidate():
@@ -6,3 +6,12 @@ async def test_SelectMultipleValidate():
     obj.options = (1, 2, 3)
     obj.value = [1, 4]
     assert obj.value == (1,)
+
+
+def test_dropdown_add():
+    dd = DropdownAdd()
+    dd.value = "abc"
+    assert "abc" in dd.options
+    assert dd.value == "abc"
+    dd.options = ()
+    assert not dd.options
